af30be558bab9b4aaeb0867f9ea631ee0872468d
TomACShaw <tomacshaw@gmail.com>
2025-07-07 17:13:59 -0400

    add nameguard to paster

--- src/ChartExtractor/ChartVisualizer/scripts/paster.py
+++ src/ChartExtractor/ChartVisualizer/scripts/paster.py
@@ -20,18 +20,23 @@ BLANK_CHART = "/home/ajax/my-venv/test_files/assets/blank_sheet.png"
 init_x = int(sys.argv[1])
 init_y = int(sys.argv[2])
 
+if __name__ == "main":
+
+
+
+
 # sheet_with_arrow = arrow.draw_arrow("/home/ajax/my-venv/test_files/assets/blank_sheet.png", 300, 300)
 
 # sheet_with_arrow = arrow.draw_diastolic_arrow(BLANK_CHART, init_x, init_y)
-sheet_with_arrow = arrow.draw_systolic_arrow(BLANK_CHART, init_x, init_y)
-sheet_with_arrow.save("test.png")
+    sheet_with_arrow = arrow.draw_systolic_arrow(BLANK_CHART, init_x, init_y)
+    sheet_with_arrow.save("test.png")
 
-for i in range(10):
-    init_x += 60
-    init_y += 40
-    sheet_with_arrow = arrow.draw_systolic_arrow("test.png", init_x, init_y)
-    sheet_with_arrow.save("test.png")
-    sheet_with_arrow = arrow.draw_circle("test.png", init_x, init_y + 80) 
-    sheet_with_arrow.save("test.png")
-    sheet_with_arrow = arrow.draw_diastolic_arrow("test.png", init_x, init_y + 160)
-    sheet_with_arrow.save("test.png")
+    for i in range(10):
+        init_x += 60
+        init_y += 40
+        sheet_with_arrow = arrow.draw_systolic_arrow("test.png", init_x, init_y)
+        sheet_with_arrow.save("test.png")
+        sheet_with_arrow = arrow.draw_circle("test.png", init_x, init_y + 80) 
+        sheet_with_arrow.save("test.png")
+        sheet_with_arrow = arrow.draw_diastolic_arrow("test.png", init_x, init_y + 160)
+        sheet_with_arrow.save("test.png")
